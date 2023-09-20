4435fab15094b17637de48603696a991ce88eb12
Ben Firshman <ben@firshman.com>
2023-09-20 10:36:43 -0700

    Add grid

--- pages/index.js
+++ pages/index.js
@@ -42,13 +42,40 @@ export default function Home() {
     }
   };
 
+  let patches = new Array(10);
+
+  for (let i = 0; i < 10; i++) {
+    patches[i] = new Array(10).fill(null);
+  }
+
+  patches[0][0] =
+    "https://replicate.delivery/pbxt/E6Ftpfi0dF1SOi4b6ltUwsfdxUf7zTQSfdhmJfRcfGTdZ88UE/out-0.png";
+
   return (
-    <div className="container max-w-2xl mx-auto p-5">
+    <div className="">
       <Head>
         <title>Replicate + Next.js</title>
       </Head>
 
-      <h1 className="py-6 text-center font-bold text-2xl">
+      <div
+        className="grid"
+        style={{
+          gridTemplateRows: "repeat(10, minmax(0, 1fr))",
+          gridTemplateColumns: "repeat(10, minmax(0, 1fr))",
+        }}
+      >
+        {patches.map((row, i) =>
+          row.map((patch, j) => {
+            return patch ? (
+              <img src={patch} className="" alt="" />
+            ) : (
+              <div className="border-right border-r border-b border-slate-200"></div>
+            );
+          })
+        )}
+      </div>
+
+      {/* <h1 className="py-6 text-center font-bold text-2xl">
         Dream something with{" "}
         <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
           SDXL
@@ -83,7 +110,7 @@ export default function Home() {
           )}
           <p className="py-3 text-sm opacity-50">status: {prediction.status}</p>
         </>
-      )}
+      )} */}
     </div>
   );
 }
