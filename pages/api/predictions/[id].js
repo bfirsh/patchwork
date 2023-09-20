d33a3b1d50d5152a9018528b9fc752d6ebfd9fa2
andreasjansson <andreas@replicate.ai>
2023-09-20 11:31:17 -0700

    Make backend work

--- pages/api/predictions/[id].js
+++ pages/api/predictions/[id].js
@@ -1,4 +1,6 @@
 import Replicate from "replicate";
+const axios = require('axios');
+const { createCanvas, loadImage } = require('canvas');
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
@@ -13,5 +15,33 @@ export default async function handler(req, res) {
     return;
   }
 
-  res.end(JSON.stringify(prediction));
+  if (prediction.status == "succeeded") {
+    res.end(JSON.stringify({
+      output: await cropBottomRight(prediction.output[0]),
+      status: prediction.status,
+    }));
+  } else {
+    res.end(JSON.stringify(prediction));
+  }
+}
+
+async function cropBottomRight(imageUrl) {
+  // Load the image
+  const img = await loadImage(imageUrl);
+
+  // Create a canvas with 768x768 dimensions
+  const canvas = createCanvas(768, 768);
+  const ctx = canvas.getContext('2d');
+
+  // Calculate the x, y coordinates for cropping the bottom right portion
+  const x = img.width - 768;
+  const y = img.height - 768;
+
+  // Draw the image onto the canvas starting at the calculated coordinates
+  ctx.drawImage(img, x, y, 768, 768, 0, 0, 768, 768);
+
+  // Convert to PNG data URL or any other format you need
+  const croppedDataUrl = canvas.toDataURL('image/png');
+
+  return croppedDataUrl;
 }
--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -1,31 +1,108 @@
 import Replicate from "replicate";
+const axios = require('axios');
+const { createCanvas, loadImage } = require('canvas');
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
+const maskSize = 256;
+const version = "c11bac58203367db93a3c552bd49a25a5418458ddffb7e90dae55780765e26d6";
 
 export default async function handler(req, res) {
+
   if (!process.env.REPLICATE_API_TOKEN) {
     throw new Error(
       "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
     );
   }
 
-  const prediction = await replicate.predictions.create({
-    // Pinned to a specific version of Stable Diffusion
-    // See https://replicate.com/stability-ai/sdxl
-    version: "2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
-
-    // This is the text prompt that will be submitted by a form on the frontend
-    input: { prompt: req.body.prompt },
-  });
-
-  if (prediction?.error) {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ detail: prediction.error }));
-    return;
+  const { prompt, neighborTop, neighborLeft } = req.body;
+  if (!neighborTop && !neighborLeft) {
+    const prediction = await replicate.predictions.create({
+      version: version,
+      input: {
+        prompt: prompt,
+        width: 768,
+        height: 768,
+      }
+    })
+    res.statusCode = 201;
+    res.end(JSON.stringify(prediction));
   }
 
+  const { image, mask, width, height } = await generateImageAndMask(neighborTop, neighborLeft);
+
+  const prediction = await replicate.predictions.create({
+    version: version,
+    input: {
+      prompt: prompt,
+      width: width,
+      height: height,
+      image: image,
+      mask: mask,
+    }
+  });
   res.statusCode = 201;
   res.end(JSON.stringify(prediction));
+}
+
+async function fetchImage(url) {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  return loadImage(Buffer.from(response.data));
+}
+
+async function generateImageAndMask(topUrl, leftUrl) {
+  let canvasWidth = 768 + maskSize;
+  let canvasHeight = 768 + maskSize;
+  if (!topUrl) {
+    canvasHeight = 768;
+  }
+  if (!leftUrl) {
+    canvasWidth = 768;
+  }
+
+  const canvas = createCanvas(canvasWidth, canvasHeight);
+  const ctx = canvas.getContext('2d');
+  const maskCanvas = createCanvas(canvasWidth, canvasHeight);
+  const maskCtx = maskCanvas.getContext('2d');
+
+  // Initialize with white background
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  maskCtx.fillStyle = 'white';
+  maskCtx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  if (topUrl && leftUrl) {
+    const topImage = await fetchImage(topUrl);
+    ctx.drawImage(topImage, 0, topImage.height - maskSize, 768, maskSize, maskSize, 0, 768, maskSize);
+    maskCtx.fillStyle = 'black';
+    maskCtx.fillRect(maskSize, 0, 768, maskSize);
+
+    const leftImage = await fetchImage(leftUrl);
+    ctx.drawImage(leftImage, leftImage.width - maskSize, 0, maskSize, 768, 0, maskSize, maskSize, 768);
+    maskCtx.fillStyle = 'black';
+    maskCtx.fillRect(0, maskSize, maskSize, 768);
+  } else if (topUrl) {
+    const topImage = await fetchImage(topUrl);
+    ctx.drawImage(topImage, 0, topImage.height - maskSize, 768, maskSize, 0, 0, 768, maskSize);
+    maskCtx.fillStyle = 'black';
+    maskCtx.fillRect(0, 0, 768, maskSize);
+  } else if (leftUrl) {
+    const leftImage = await fetchImage(leftUrl);
+    ctx.drawImage(leftImage, leftImage.width - maskSize, 0, maskSize, 768, 0, 0, maskSize, 768);
+    maskCtx.fillStyle = 'black';
+    maskCtx.fillRect(0, 0, maskSize, 768);
+  }
+
+  /*
+  const imageBuffer = canvas.toBuffer('image/png');
+  const maskBuffer = maskCanvas.toBuffer('image/png');
+  fs.writeFileSync('./image.png', imageBuffer);
+  fs.writeFileSync('./mask.png', maskBuffer);
+  */
+
+  const imageDataUrl = canvas.toDataURL('image/png');
+  const maskDataUrl = maskCanvas.toDataURL('image/png');
+
+  return { image: imageDataUrl, mask: maskDataUrl, width: canvasWidth, height: canvasHeight };
 }
